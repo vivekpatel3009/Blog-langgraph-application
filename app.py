f7e5d2e7f4c08c79dbf24bc4ab85210144ccef5a
Vivek <vivek@gmail.com>
2026-05-19 18:22:38 +0530

    update port change

--- app.py
+++ app.py
@@ -39,5 +39,6 @@ async def create_blogs(request:Request):
     return {"data":state}
 
 if __name__=="__main__":
-    uvicorn.run("app:app",host="0.0.0.0",port=8000,reload=True)
+    uvicorn.run("app:app",host="0.0.0.0",
+        port=int(os.environ.get("PORT", 8000)))
 
